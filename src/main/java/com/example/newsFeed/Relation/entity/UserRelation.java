0e16ad71c5e942dbfb487ecf75a2e06e2e666eff
rlgkghkd <rlgkghkd@gmail.com>
2025-04-07 14:47:59 +0900

    choer:branch

--- src/main/java/com/example/newsFeed/Relation/entity/UserRelation.java
+++ src/main/java/com/example/newsFeed/Relation/entity/UserRelation.java
@@ -1,5 +1,26 @@
 package com.example.newsFeed.Relation.entity;
 
-public class UserRelation {
+import com.example.newsFeed.Users.entity.User;
+import com.example.newsFeed.entity.Base;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "userT")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRelation extends Base {
+    @ManyToMany
+    private List<User> follower;
+    @ManyToMany
+    private List<User> following;
+
 
 }
