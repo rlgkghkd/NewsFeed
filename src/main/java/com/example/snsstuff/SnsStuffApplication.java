746d2f1ddf350fd93756b67a9b38fda960cd55e8
rlgkghkd <rlgkghkd@gmail.com>
2025-04-07 11:23:10 +0900

    build: 프로젝트 세팅

---
+++ src/main/java/com/example/snsstuff/SnsStuffApplication.java
@@ -0,0 +1,13 @@
+package com.example.snsstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsStuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsStuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snsstuff/SnsStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snsstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsStuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
