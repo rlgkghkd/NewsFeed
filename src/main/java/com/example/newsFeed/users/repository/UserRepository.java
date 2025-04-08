50a21ad3879a0a2892e8ffbc91a4471b29c7c33f
crocusia <132359536+crocusia@users.noreply.github.com>
2025-04-08 15:49:05 +0900

    feat : 병합 후 누락된 UserRepository 추가

---
+++ src/main/java/com/example/newsFeed/users/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.newsFeed.users.repository;
+
+import com.example.newsFeed.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    default User findByIdOrElseThrow(Long id){
+        return findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다." + id));
+    }
+}
