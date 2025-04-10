b446257b80740abe39deeb80cb31c1be1d5ed6f2
prem3dev <ecstasy1508@gmail.com>
2025-04-10 13:53:03 +0900

    :adhesive_bandage: 로그인api의 URI를 따로 지정, 로그아웃 api 구현
    로그인 api URI = "/api/login"
    로그아웃 api URI = "/api/logout"

--- src/main/java/com/example/newsFeed/jwt/controller/Controller.java
+++ src/main/java/com/example/newsFeed/jwt/controller/Controller.java
@@ -5,8 +5,10 @@ import com.example.newsFeed.jwt.utils.TokenUtils;
 import com.example.newsFeed.jwt.dto.LoginRequestDto;
 import com.example.newsFeed.jwt.dto.LoginResponseDto;
 import com.example.newsFeed.users.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +18,13 @@ import java.time.Duration;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/login")
 public class Controller {
 
     private final TokenUtils tokenUtils;
     private final UserService userService;
     private final TokenRedisService tokenRedisService;
 
-    @PostMapping
+    @PostMapping("/api/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto requestDto) {
 
         Long id = userService.login(requestDto);
@@ -57,5 +58,34 @@ public class Controller {
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(new LoginResponseDto("로그인 되었습니다."));
+    }
+
+    @DeleteMapping("/api/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        tokenRedisService.deleteTokenRedis(request);
+
+        // 쿠키 삭제
+        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        //헤더에 기간이 0인 쿠키들 등록
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+        return ResponseEntity.ok().headers(headers).body("정상적으로 로그아웃 되셨습니다.");
     }
 }
