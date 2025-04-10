bd09cb22f7053a58b16e7137635d06dc749fe19e
prem3dev <ecstasy1508@gmail.com>
2025-04-10 11:38:29 +0900

    :recycle: TokenRedis class 설정

--- src/main/java/com/example/newsFeed/jwt/entity/TokenRedis.java
+++ src/main/java/com/example/newsFeed/jwt/entity/TokenRedis.java
@@ -1,5 +1,28 @@
 package com.example.newsFeed.jwt.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class TokenRedis {
 
+    @Id
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String refreshToken;
+
+    public TokenRedis (Long userId, String refreshToken){
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public static TokenRedis toEntity(Long userId, String refreshToken){
+
+        return new TokenRedis(userId, refreshToken);
+    }
 }
