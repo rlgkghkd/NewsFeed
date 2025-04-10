ed2868f93b8e5445d34a28c6ac152c237aa35d7b
Woo <swy831@gmail.com>
2025-04-10 15:05:40 +0900

    feat : Comment 추가

--- src/main/java/com/example/newsFeed/boards/dto/BoardResponseDto.java
+++ src/main/java/com/example/newsFeed/boards/dto/BoardResponseDto.java
@@ -4,15 +4,22 @@ import com.example.newsFeed.boards.entity.Board;
 import com.example.newsFeed.users.entity.User;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 public class BoardResponseDto {
     private final String title;
     private final String contents;
     private final String userName;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
 
     public BoardResponseDto(Board board) {
         this.title = board.getTitle();
         this.contents = board.getContents();
         this.userName = board.getUser().getName();
+        this.createdAt = board.getCreatedAt();
+        this.modifiedAt = board.getModifiedAt();
     }
 }
