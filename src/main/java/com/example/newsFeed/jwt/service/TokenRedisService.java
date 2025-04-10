435a71ddd4ac5f36b3dfb3ee2dfa9ba2fb4b63c1
prem3dev <ecstasy1508@gmail.com>
2025-04-10 14:08:11 +0900

    :sparkles: db에서 로그인된 유저의 TokenRedis를 제거하는 메서드 구현
    로그아웃시 db에서 TokenRedis를 제거함으로써 refreshToken을 제거한다.

--- src/main/java/com/example/newsFeed/jwt/service/TokenRedisService.java
+++ src/main/java/com/example/newsFeed/jwt/service/TokenRedisService.java
@@ -2,8 +2,12 @@ package com.example.newsFeed.jwt.service;
 
 import com.example.newsFeed.jwt.entity.TokenRedis;
 import com.example.newsFeed.jwt.repository.TokenRedisRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.example.newsFeed.jwt.utils.TokenUtils.getAccessToken;
+import static com.example.newsFeed.jwt.utils.TokenUtils.getUserIdFromToken;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +18,11 @@ public class TokenRedisService {
     public void saveRefreshInDb(Long userId, String refreshToken) {
         TokenRedis tokenRedis = TokenRedis.toEntity(userId, refreshToken);
         tokenRedisRepository.save(tokenRedis);
+    }
+
+    public void deleteTokenRedisInDb(HttpServletRequest request) {
+        String accessToken = getAccessToken(request);
+        Long id = getUserIdFromToken(accessToken);
+        tokenRedisRepository.deleteById(id);
     }
 }
