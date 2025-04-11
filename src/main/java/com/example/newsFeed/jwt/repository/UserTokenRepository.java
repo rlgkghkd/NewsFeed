064205851e26636c96561bda7181538d3fcdd249
prem3dev <ecstasy1508@gmail.com>
2025-04-11 20:27:09 +0900

    style(Jwt): UserTokenRepository 주석 추가

--- src/main/java/com/example/newsFeed/jwt/repository/UserTokenRepository.java
+++ src/main/java/com/example/newsFeed/jwt/repository/UserTokenRepository.java
@@ -9,11 +9,11 @@ import java.util.Optional;
 
 public interface UserTokenRepository extends JpaRepository<UserToken, Long> {
 
+    //refreshToken으로 UserToken을 조회
     Optional<UserToken> findByRefreshToken(String refreshToken);
 
+    //refreshToken으로 UserToken을 조회하고,존재하지 않으면 CustomException(Errors.USER_TOKEN_NOT_FOUND) 예외 발생
     default UserToken findUserTokenByRefreshTokenOrElseThrow(String refreshToken) {
         return findByRefreshToken(refreshToken).orElseThrow(()-> new CustomException(Errors.USER_TOKEN_NOT_FOUND));
     }
-
-    void deleteByRefreshToken(String refreshToken);
 }
