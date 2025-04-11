113af0c2df5ab702cd1199a45e431a86692bbea0
prem3dev <ecstasy1508@gmail.com>
2025-04-11 10:52:55 +0900

    feat(global): Errors jwt관련 예외와 메서드 추가
    - jwt관련 예외 추가
    - 예외 타입에 따라 대앙흐난 Errors enum을 반환하는 메서드 추가

--- src/main/java/com/example/newsFeed/global/exception/Errors.java
+++ src/main/java/com/example/newsFeed/global/exception/Errors.java
@@ -1,7 +1,12 @@
 package com.example.newsFeed.global.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.dao.DataAccessException;
 
 @AllArgsConstructor
 @Getter
@@ -27,11 +32,37 @@ public enum Errors {
     // Follower
     FOLLOWER_NOT_FOUND(404, "Not Found", "F001", "Follower Not Found"),
 
-
     // TokenRedis
-    TOKEN_REDIS_NOT_FOUND(404, "Not Found", "T001", "Token Redis Not Found");
+    TOKEN_REDIS_NOT_FOUND(404, "Not Found", "T001", "Token Redis Not Found"),
+
+    // Jwt
+    JWT_UNKNOWN(403, "Forbidden", "J000", "알 수 없는 JWT 예외입니다."),
+    JWT_SIGNATURE_INVALID(403, "Forbidden", "J001", "JWT 서명이 잘못되었습니다."),
+    JWT_MALFORMED(403, "Forbidden", "J002", "형식이 잘못된 JWT입니다."),
+    JWT_UNSUPPORTED(403, "Forbidden", "J003", "지원하지 않는 형식의 JWT입니다."),
+    JWT_ILLEGAL(403, "Forbidden", "J004", "JWT 형식이 올바르지 않습니다."),
+    JWT_INVALID(403, "Forbidden", "J005", "유효하지 않은 JWT입니다."),
+    JWT_EXPIRED(403, "Forbidden", "J006", "JWT가 만료되었습니다."),
+    // Db
+    DB_ERROR(500, "Internal Server Error", "D001", "DB 접근 중 문제가 발생했습니다.");
+
     private final int status;
     private final String error;
     private final String code;
     private final String message;
+
+    /**
+     * 예외 타입에 따라 대응하는 Errors enum 반환
+     */
+    public static Errors fromException(Exception e) {
+        if (e instanceof SecurityException) return JWT_SIGNATURE_INVALID;
+        if (e instanceof MalformedJwtException) return JWT_MALFORMED;
+        if (e instanceof UnsupportedJwtException) return JWT_UNSUPPORTED;
+        if (e instanceof IllegalArgumentException) return JWT_ILLEGAL;
+        if (e instanceof ExpiredJwtException) return JWT_EXPIRED;
+        if (e instanceof JwtException) return JWT_INVALID;
+        if (e instanceof DataAccessException) return DB_ERROR;
+        return JWT_UNKNOWN; // 기본값
+    }
+
 }
