7f2189a6e95be1f44316b8f70818bebb6df84f41
prem3dev <ecstasy1508@gmail.com>
2025-04-11 11:06:01 +0900

    refactor(Jwt): JwtFilter에서 TokenRedisService 사용을 위해 의존성 주입한 것에 따른 WebConfig에서의 의존성 추가

--- src/main/java/com/example/newsFeed/global/filter/WebConfig.java
+++ src/main/java/com/example/newsFeed/global/filter/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.newsFeed.global.filter;
 
+import com.example.newsFeed.jwt.service.TokenRedisService;
 import com.example.newsFeed.jwt.utils.TokenUtils;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -11,16 +12,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final TokenUtils tokenUtils;
+    private final TokenRedisService tokenRedisService;
 
-    public WebConfig(TokenUtils tokenUtils) {
+
+    public WebConfig(TokenUtils tokenUtils, TokenRedisService tokenRedisService) {
         this.tokenUtils = tokenUtils;
+        this.tokenRedisService = tokenRedisService;
     }
 
     @Bean
     public FilterRegistrationBean JwtFilter() {
 
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new JwtFilter(tokenUtils));
+        filterRegistrationBean.setFilter(new JwtFilter(tokenUtils, tokenRedisService));
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
 
