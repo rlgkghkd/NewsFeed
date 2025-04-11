ddb67bc11af455bf35598eb4f339231e8528f343
prem3dev <ecstasy1508@gmail.com>
2025-04-11 10:57:13 +0900

    feat(Jwt): @Email 추가

--- src/main/java/com/example/newsFeed/jwt/dto/LoginRequestDto.java
+++ src/main/java/com/example/newsFeed/jwt/dto/LoginRequestDto.java
@@ -1,16 +1,16 @@
 package com.example.newsFeed.jwt.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
 public class LoginRequestDto {
 
    @NotBlank(message = "이메일을 필수로 입력해주세요.")
+   @Email(message = "올바른 이메일 주소를 입력하세요.")
    private final String email;
 
    @NotBlank(message = "비밀번호를 필수로 입력해주세요")
